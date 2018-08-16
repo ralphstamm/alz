33d0e94cb10e70c65eb27b4186238c4ebc06ea91
ralphstamm <github@ralphstamm.com>
2018-08-16 21:38:40 +0200

    Create test.js

---
+++ adobe/analytics/DataConnectors/DoubleClick/test.js
@@ -0,0 +1,9 @@
+(function(w, d, s) {
+    var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s);
+    j.async = true;
+  	r = Math.floor((Math.random() * 10000000) + 1);
+    j.src =
+        'https://fls.doubleclick.net/json?spot=6774922&src=&var=resp&host=integrate.112.2o7.net%2Fdfa_echo%3Fvar%3Dresp%26AQE%3D1%26A2S%3D1&ord='+r
+    f.parentNode.insertBefore(j, f);
+})(window, document, 'script');
