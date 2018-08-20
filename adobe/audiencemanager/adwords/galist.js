1a9b05826a6d464fa1889bd8bbb58321bdd93d0d
ralphstamm <github@ralphstamm.com>
2018-08-20 20:48:02 +0200

    Create galist.js

---
+++ adobe/audiencemanager/adwords/galist.js
@@ -0,0 +1,23 @@
+<!-- Google Code for Adobe Audience Manager Test - Allianz Car Pages -->
+<!-- Google Code for Remarketing Tag -->
+<!--------------------------------------------------
+Remarketing tags may not be associated with personally identifiable information or placed on pages related to sensitive categories. See more information and instructions on how to setup the tag on: http://google.com/ads/remarketingsetup
+--------------------------------------------------->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 1036384049;
+var google_conversion_label = "JUUyCMyG5oYBELHul-4D";
+var google_custom_params = window.google_tag_params;
+var google_remarketing_only = true;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/1036384049/?value=1.00&amp;currency_code=CHF&amp;label=JUUyCMyG5oYBELHul-4D&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>
+
+
+//googleads.g.doubleclick.net/pagead/viewthroughconversion/1036384049/?value=1.00&currency_code=CHF&label=JUUyCMyG5oYBELHul-4D&guid=ON&script=0
