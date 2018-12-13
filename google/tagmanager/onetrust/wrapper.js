615a4d33883e61cb2241c25d831760db4262d71f
ralphstamm <6294611+ralphstamm@users.noreply.github.com>
2018-12-13 08:40:21 +0100

    Create wrapper.js

---
+++ google/tagmanager/onetrust/wrapper.js
@@ -0,0 +1,7 @@
+ <script src="https://cdn.cookielaw.org/langswitch/d342f7b9-493a-4ada-9f06-fb4cf7c767a6.js" type="text/javascript" charset="UTF-8"></script>
+
+ <script type="text/javascript">
+
+ function OptanonWrapper() { }
+
+ </script>
